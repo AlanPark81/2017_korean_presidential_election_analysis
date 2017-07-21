57eb8c66c25a42e9aeb41e8b84d8364cc90ebb3c
Alan Park <alanpark.jaewoo@gmail.com>
2017-07-21 13:53:06 +0900

    [Python] add functionalities computing equalised values and their variances

--- 2017_kpea.py
+++ 2017_kpea.py
@@ -4,17 +4,18 @@ import pandas
 
 raw_data=genfromtxt('PrElecData1.csv', delimiter=',')
 
-for row in raw_data:
-	print(', '+str(row))
-
 dataframe=pandas.DataFrame(raw_data)
-#print(dataframe.reindex_axis(labels=['문재인', '홍준표','안철수','유승민','심상정'], axis='columns', copy=True ))
-print(dataframe)
-#print(dataframe1)
 dfrank=dataframe.rank(axis=0, ascending=True, method='min', na_option='bottom')
-#rank.reindex(columns=['문재인', '홍준표','안철수','유승민','심상정'] )
-print(dfrank)
 print("The Correlations of Raw Data ")
 print(dataframe.corr())
 print("The Correlations of Ranks")
 print(dfrank.corr())
+dfmax=dataframe.max(axis=0)
+dfmin=dataframe.min(axis=0)
+dfgap=(dfmax-dfmin)
+dfmul=100/dfgap
+dfeql=(dataframe-dfmin)*dfmul
+print('Equalised values')
+print(dfeql)
+print('Equalised values\' variances')
+print(dfeql.var(axis=0))
